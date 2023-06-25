b250069a2e9ab0bc0b6993edc7a5ad4b05565e35
Betti <patrick.betti@sony.com>
2023-06-25 15:02:16 -0700

    Add test files, conftest file for pytest flask, Actions file

--- quote_disp/tests/test_app.py
+++ quote_disp/tests/test_app.py
@@ -1,4 +1,9 @@
-def test_health():
-    assert add(1, 1) == 2
+import json
+
+def test_get_quote_route(app, client):
+    res = client.get('/')
+    assert res.status_code == 200
+    expected = {'hello': 'world'}
+    assert expected == json.loads(res.get_data(as_text=True))
 
 
