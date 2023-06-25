7575304cad2256a66bfdbbd218690a9bc8b36652
Betti <patrick.betti@sony.com>
2023-06-25 16:29:44 -0700

    add quote gen tests, and index page tests

--- quote_gen/tests/test_app.py
+++ quote_gen/tests/test_app.py
@@ -9,16 +9,17 @@ def test_get_index_route(app, client):
     res = client.get('/get_quote')
     assert res.status_code == 200
     expected = {'hello': 'world'}
-    assert b"Quote Display Service" in res.data
+    assert b"Quote Gen Service" in res.data
 
 def test_get_quote_route(app, client):
     """
-    GIVEN quote display service
-    WHEN '/get_quote' page is requested (GET)
+    GIVEN quote gen service
+    WHEN '/quote' page is requested (GET)
     THEN check that a quote is displayed
     """
-    res = client.get('/get_quote')
-    assert res.status_code == 500
+    res = client.get('/quote')
+    assert res.status_code == 200
     expected = {'hello': 'world'}
+    assert b"quote -" in res.data
 
 
